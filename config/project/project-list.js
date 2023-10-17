8cb2a0926bcaaf79499f37b897c1d2d6fe4b7f34
liyanqing <liyanqing@jiaxincloud.com>
2023-10-17 16:27:03 +0800

    新增文件

--- config/project/project-list.js
+++ config/project/project-list.js
@@ -1,15 +1,15 @@
 export default [
    {
-    enName: "admin",
-    cnName: "admin",
-    root: "admin",
-    projectKey: "admin",
+    enName: "threejs",
+    cnName: "threejs",
+    root: "threejs",
+    projectKey: "threejs",
   },
    {
-    enName: "admin1",
-    cnName: "admin1",
-    root: "admin1",
-    projectKey: "admin1",
+    enName: "sence",
+    cnName: "sence",
+    root: "sence",
+    projectKey: "sence",
   },
 
 ]
